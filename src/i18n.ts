ec37dd01caf2cea67af9a8d82480bf7028674267
as <banana.ayato@icloud.com>
2026-06-26 21:56:50 +0900

    chore: add page-specific titles to src/locales/ja.json

---
+++ src/i18n.ts
@@ -0,0 +1,8 @@
+import { createI18n } from 'vue-i18n'
+import ja from './locales/ja.json'
+
+export const i18n = createI18n({
+  legacy: false,
+  locale: 'ja',
+  messages: { ja },
+})
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { i18n } from '../i18n'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -10,6 +11,12 @@ const router = createRouter({
       component: HomeView,
     },
   ],
+})
+
+router.afterEach((to) => {
+  const key = to.meta.title as string | undefined
+  const suffix = i18n.global.t('pageTitle.suffix')
+  document.title = key ? `${i18n.global.t(key)} | ${suffix}` : suffix
 })
 
 export default router
