73e6621a2cf28a4b50056ea7bbd6d45ae5a952e0
as <banana.ayato@icloud.com>
2026-07-11 22:12:22 +0900

    fix(status): set signage page title

--- apps/status/src/main.ts
+++ apps/status/src/main.ts
@@ -4,8 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import SignageView from './components/SignageView.vue'
 
-const app = createApp(
-  window.location.pathname.replace(/\/$/, '') === '/signage' ? SignageView : App,
-)
+const isSignage = window.location.pathname.replace(/\/$/, '') === '/signage'
+
+if (isSignage) document.title = 'サイネージ | 八宝祭 - LiSA Papillon Festival 2026'
+
+const app = createApp(isSignage ? SignageView : App)
 
 app.mount('#app')
